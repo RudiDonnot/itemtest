76cada036e28dcf2b902c44d5214578b1a20979d
Rudi Donnot <rudi.donnot@gmail.com>
2024-12-09 17:16:10 +0100

    mise à jour 4

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const path = require('path');
 const app = express();
 
 var publicDir = path.join(__dirname, 'public');
-app.use("/public/", express.static(publicDir));
+app.use("./public/", express.static(publicDir));
 
 /*
   -> IMPORTANT
   -> it is a security risk to serve node_modules via express. You should use a bundler like webpack or browserify
 */
 var nodeModulesDir = path.join(__dirname, 'node_modules');
-app.use('/node_modules/', express.static(nodeModulesDir)); 
+app.use('./node_modules/', express.static(nodeModulesDir)); 
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+app.get("./", function (req, res) {
+  res.sendFile(__dirname + "./index.html");
 });
 
 app.listen(3000, function () {
